986250195229249200a18e02e00dafba18e9db27
injinjeong <snowwwwww@naver.com>
2022-10-15 21:18:51 +0900

    kotlin tdd 추가중 step03

--- src/test/kotlin/com/group/tddStudy/domain/Study.kt
+++ src/test/kotlin/com/group/tddStudy/domain/Study.kt
@@ -2,30 +2,17 @@ package com.group.tddStudy.domain
 
 import java.time.LocalDateTime
 
-class Study {
-    var status: StudyStatus? = StudyStatus.DRAFT
-    var limit: Int
+class Study(
+    var limit: Int,
     var name: String? = null
+) {
+    init {
+        if(limit <= 0) throw IllegalArgumentException("limit 은 0 보다 커야 합니다.")
+    }
+    var status: StudyStatus? = StudyStatus.DRAFT
+
     private var openedDateTime: LocalDateTime? = null
     private val owner: Member? = null
-
-//    constructor(limit: Int, name: String?) {
-//        this.limit = limit
-//        this.name = name
-//    }
-
-    constructor(limit: Int) {
-        if(limit <= 0) throw IllegalArgumentException("limit 은 0 보다 커야 합니다.")
-        this.limit = limit
-    }
-
-    override fun toString(): String {
-        return "Study{" +
-                "status=" + status +
-                ", limit=" + limit +
-                ", name='" + name + '\'' +
-                '}'
-    }
 
     fun open() {
         openedDateTime = LocalDateTime.now()
